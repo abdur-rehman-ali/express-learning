161a9d84a49dd4ef506653f989c20442c407b509
abdur-rehman-ali <alijoyia911@gmail.com>
2023-01-29 23:56:58 +0500

    Wrap the posts action in asyncHandler

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,64 +1,46 @@
 import Post from '../models/post.js'
 
+import asyncHandler from '../middlewares/asyncHandler.js'
+
 class PostsController {
-  static getPosts = async (req, res) => {
-    try {
-      const posts = await Post.find()
-      res.status(200).json({ status: 'success', data: posts })
-    } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message })
-    }
-  }
+  static getPosts = asyncHandler(async (req, res) => {
+    const posts = await Post.find()
+    res.status(200).json({ status: 'success', count: posts.length, data: posts })
+  })
 
-  static getPost = async (req, res) => {
-    try {
-      const { id } = req.params
-      const post = await Post.findById(id)
-      if (!post) {
-        return res.status(404).json({ status: 'error', message: `No post exist with id: ${id}` })
-      }
-      res.status(200).json({ status: 'success', data: post })
-    } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message })
+  static getPost = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const post = await Post.findById(id)
+    if (!post) {
+      return res.status(404).json({ status: 'error', message: `No post exist with id: ${id}` })
     }
-  }
+    res.status(200).json({ status: 'success', data: post })
+  })
 
-  static createPost = async (req, res) => {
-    try {
-      const { title, description } = req.body
-      const post = await Post.create({ title, description })
-      res.status(201).json({ status: 'success', message: 'Post created successfully!!!', data: post })
-    } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message })
-    }
-  }
+  static createPost = asyncHandler(async (req, res) => {
+    const { title, description } = req.body
+    const post = await Post.create({ title, description })
+    res.status(201).json({ status: 'success', message: 'Post created successfully!!!', data: post })
+  })
 
-  static editPost = async (req, res) => {
-    try {
-      const { id } = req.params
-      const { title, description } = req.body
-      const post = await Post.findByIdAndUpdate(id, { title, description }, { runValidators: true, new: true })
-      if (!post) {
-        return res.status(404).json({ status: 'error', message: `No post exist with id: ${id}` })
-      }
-      res.status(200).json({ status: 'success', message: 'Post updated successfully!!!', data: post })
-    } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message })
+  static editPost = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const { title, description } = req.body
+    const post = await Post.findByIdAndUpdate(id, { title, description }, { runValidators: true, new: true })
+    if (!post) {
+      return res.status(404).json({ status: 'error', message: `No post exist with id: ${id}` })
     }
-  }
+    res.status(200).json({ status: 'success', message: 'Post updated successfully!!!', data: post })
+  })
 
-  static deletePost = async (req, res) => {
-    try {
-      const { id } = req.params
-      const post = await Post.findByIdAndDelete(id)
-      if (!post) {
-        return res.status(404).json({ status: 'error', message: `No post exist with id: ${id}` })
-      }
-      res.status(200).json({ status: 'success', message: 'Post deleted successfully!!!' })
-    } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message })
+  static deletePost = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const post = await Post.findByIdAndDelete(id)
+    if (!post) {
+      return res.status(404).json({ status: 'error', message: `No post exist with id: ${id}` })
     }
-  }
+    res.status(200).json({ status: 'success', message: 'Post deleted successfully!!!' })
+  })
 }
 
 export default PostsController
